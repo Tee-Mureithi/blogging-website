87177f4de4414417c61558c2ac599b91076a8023
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-03-18 02:05:38 +0300

    adds text

--- app/main/views.py
+++ app/main/views.py
@@ -17,9 +17,9 @@ def index():
     title = 'Home - Welcome to Blogging Website'
 
     # Getting reviews by category
-    lifestyle_blog = Blog.get_blogs('lifestle')
+    lifestyle_blog = Blog.get_blogs('lifestyle')
     motivation_blog = Blog.get_blogs('motivation')
-    mentalhealth_blog = Blog.blogs('mentalhealth')
+    mentalhealth_blog = Blog.get_blogs('mentalhealth')
 
 
     return render_template('index.html',title = title, lifestle = lifestyle_blog, motivation = motivation_blog, mentalhealth = mentalhealth_blog)
@@ -28,7 +28,7 @@ def index():
 def profile(uname):
     user = User.query.filter_by(username = uname).first()
     blogs_count = Blog.count_blogs(uname)
-    user_joined = user.date_joined.strftime('%b %d, %Y')
+    user_joined = User.date_joined.strftime('%b %d, %Y')
 
     if user is None:
         abort(404)
